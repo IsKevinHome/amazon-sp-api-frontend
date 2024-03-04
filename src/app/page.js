84929862d8f9410eccdb908e544257b934467897
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-03-03 20:32:11 -0800

    update Oauth URI

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className='h-screen flex items-center justify-center'>
       <a
-        href='https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.35f695c4-f99d-4526-8d38-37f0ca9d99dd&version=beta'
+        href='https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.35f695c4-f99d-4526-8d38-37f0ca9d99dd&version=beta&state=stateexample'
         className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
       >
         Authorize
