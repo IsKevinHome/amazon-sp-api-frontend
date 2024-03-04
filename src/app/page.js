c1d467fea6465a20e93760b87664c21c0dbaf038
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-03-03 19:29:07 -0800

    add url

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 export default function Page() {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
+      <a
+        href='https://sellercentral.amazon.com/apps/authorize/consent?application_id=amzn1.sp.solution.35f695c4-f99d-4526-8d38-37f0ca9d99dd&version=beta'
+        className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+      >
         Authorize
-      </button>
+      </a>
     </div>
   );
 }
